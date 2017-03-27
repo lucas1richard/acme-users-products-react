64d293402210a955ba8674e528ba184166f8f081
lucas1richard <=>
2017-03-27 17:04:26 -0400

    convert jest to mocha

--- browser/test/NavBar.test.js
+++ browser/test/NavBar.test.js
@@ -1,5 +1,7 @@
 import React  from 'react';
 import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
 import NavBar from '../react/NavBar';
 
 const tabs = [ { id: 1, name: 'Users', isActive: true }, { id: 2, name: 'Products', isActive: false }];
@@ -18,25 +20,40 @@ const nav = shallow(
 
 const { props } = nav.unrendered;
 
-test('Elements', () => {
-  expect(nav.type()).toEqual('nav');
-  expect(nav.children().length).toBe(1);
-  expect(nav.children().children().length).toBe(tabs.length);
-  expect(nav.children().children().children().length).toBe(tabs.length);
-});
+describe('<NavBar /> testing', () => {
+  describe('Elements', () => {
+    it('is a nav element', () => {
+      expect(nav.type()).to.equal('nav');
+    });
+    it('has two children', () => {
+      expect(nav.children().children().length).to.equal(tabs.length);
+    });
+    it('each child has only one child', () => {
+      expect(nav.children().children().children().length).to.equal(tabs.length);
+    });
+  });
 
-test('Props', () => {
-  expect(props.tabs).toBeTruthy();
-  expect(Array.isArray(props.tabs)).toBeTruthy();
-  expect(props.handleTabChange).toBeTruthy();
-  expect(typeof props.handleTabChange).toEqual('function');
-  expect(props.numUsers).toBeTruthy();
-  expect(typeof props.numUsers).toEqual('number');
-  expect(props.numProducts).toBeTruthy();
-  expect(typeof props.numProducts).toEqual('number');
-});
+  describe('Props', () => {
+    it('has a tabs property which is an array', () => {
+      expect(props.tabs).to.be.ok;
+      expect(Array.isArray(props.tabs)).to.be.ok;
+    });
+    it('has a handleTabChange property which is a function', () => {
+      expect(props.handleTabChange).to.be.ok;
+      expect(typeof props.handleTabChange).to.equal('function');
+    });
+    it('has a numUsers property which is a number', () => {
+      expect(props.numUsers).to.be.ok;
+      expect(typeof props.numUsers).to.equal('number');
+    });
+    it('has a numProducts property which is a number', () => {
+      expect(props.numProducts).to.be.ok;
+      expect(typeof props.numProducts).to.equal('number');
+    });
+  });
 
-test('children text', () => {
-  console.dir(nav.children().nodes);
-  expect(true).toBeTruthy();
+  describe('children text', () => {
+    console.dir(nav.children().children());
+    expect(true).to.be.ok;
+  });
 });
